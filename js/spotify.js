d82a03e7db4bb79de402025a9664a73d70cafe64
Brendan Magee <bmagee@gmail.com>
2016-04-14 14:28:21 -0400

    have the beginnings of a functioning AJAX call

--- js/spotify.js
+++ js/spotify.js
@@ -1,6 +1,18 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+$(document).ready(function(){
+  var url = "https://api.spotify.com/v1/search?q=Rush&type=artist";
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){
+    console.log(response);
+  }).fail(function(err){
+    console.log(err);
+  });
 
+});
 
 function searchByArtist(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
